4f0a88ced157ebb4203da2858be6ffbbb7269d39
anshu <anshu.stackcraft@gmail.com>
2026-01-18 18:56:52 +0530

    updatemistack

--- backend/store/serializers.py
+++ backend/store/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import Category, Product, UserProfile, Order, OrderItem, Cart, CartItem,
+from .models import Category, Product, UserProfile, Order, OrderItem, Cart, CartItem
 
 
 class CategorySerializer(serializers.ModelSerializer):
